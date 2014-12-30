093eb7be3de7d93a0665cd4a0e182e1792fc834b
Christopher Dunn <cdunn2001@gmail.com>
2014-12-30 16:17:50 -0600

    AddRand

--- py/sandbox.py
+++ py/sandbox.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 from __future__ import print_function
 from contextlib import contextmanager
-import os, sys
+import os, sys, random
 
 # git-some dir:
 gsd = os.path.join('.git', 'some')
@@ -33,7 +33,12 @@ def System(cmd):
 def SvnCreate(dire):
     System('svnadmin create --fs-type fsfs %s' %dire)
     return os.path.abspath(dire)
-def Sandbox(dire, proj):
+class GitSome(object):
+    def __init__(self, gitdire, svndirmap):
+        self.gitdire = gitdire
+        self.svndbdirmap = svndirmap
+        self.svndefault = svndirmap.keys()[0]
+def CreateSandbox(dire, proj):
     if os.path.exists(dire):
         cmd = 'rm -rf %s' %dire
         msg = cmd + '?'
@@ -47,8 +52,21 @@ def Sandbox(dire, proj):
         with mkdir(proj):
             System('git init')
             with mkdir(gsd):
-                System('svn checkout file://%s .' %svndb)
+                svn1 = 'svn1'
+                System('svn checkout file://%s %s' %(svndb, svn1))
+    return GitSome(os.path.join(dire, proj), {'svn1': svndb})
+def AddRand(gs, size):
+    filename = 'foo-%000d' %random.randrange(10)
+    svnpath = os.path.join(gs.gitdire, gsd, gs.svndefault, filename)
+    print('Write %d bytes into %r' %(size, svnpath))
+    with open(svnpath, 'w') as f:
+        f.write(b'x'*size)
+    #with cd(gs.
+def AddRands(gs, n, size):
+    for i in range(n):
+        AddRand(gs, size)
 def Main(prog, dire, proj):
-    Sandbox(dire, proj)
+    gs = CreateSandbox(dire, proj)
+    AddRand(gs, 1)
 if __name__=="__main__":
     Main(*sys.argv)
